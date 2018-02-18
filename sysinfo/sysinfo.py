a826eb6a2faac2c886821031e22cf7b590148ec1
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2018-02-18 10:30:41 +0000

    Changed the output string a little

--- sysinfo/sysinfo.py
+++ sysinfo/sysinfo.py
@@ -8,7 +8,7 @@ import shutil
 def get_platform_info():
     platformInfo = platform.platform() #Will print platform information
     total, used, free = shutil.disk_usage(__file__)
-    return '{}\ntotal {}, used {}, free {}: '.format(platformInfo, total, used, free)
+    return 'The platform is: {}\n The memory usages are: total {}, used {}, free {}: '.format(platformInfo, total, used, free)
 
 if __name__ == '__main__':
     get_platform_info()
